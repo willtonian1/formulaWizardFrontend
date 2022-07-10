a2b3d185fcff6aa66bbb5a1852d41266dcb53ce9
Tom Williams <77905483+willtonian1@users.noreply.github.com>
2022-07-10 17:35:47 +0000

    New Inputs

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
 
 axios.get('https://Toms-Flask-App.193ftgw.repl.co').then(resp => {
+	data = resp.data;
+   data2 = data.replace("[", "");
+	 data3 = data2.replace("]", "");
 
-    console.log(resp.data);
+	name1 = document.getElementById("name1")
+	name1.innerHTML = data3
+		
 });
 
 var payload;
@@ -10,8 +15,8 @@ function doPostRequest() {
 
     //let payload = { name: 'SF21', year: '2022' };
 	 var x = document.getElementById("myText").value;
-
-	let y = {name: String(x), year: "2022", image : String(x) + ".jpg"};
+	var z = document.getElementById("myYear").value;
+	let y = {name: String(x), year: String(z), image : String(x) + ".jpg"};
 	console.log(y)
 	payload = y;
 
