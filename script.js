3d2188e3e8ea1b7134f61d69b933050cf5c083a9
willtonian1 <77905483+willtonian1@users.noreply.github.com>
2022-08-25 18:12:22 +0100

    new routes to backend

--- script.js
+++ script.js
@@ -6,41 +6,41 @@ image2 = document.getElementById("image2")
 
 
 function go() {
-	axios.get('https://Toms-Flask-App.193ftgw.repl.co').then(resp => {
-		data = resp.data;
-		data2 = data.replace("[", "");
-		data3 = data2.replace("]", "");
+    axios.get('https://Toms-Flask-App.193ftgw.repl.co').then(resp => {
+        data = resp.data;
+        data2 = data.replace("[", "");
+        data3 = data2.replace("]", "");
 
-		dataList = data3.split("},")
+        dataList = data3.split("},")
 
-		elements_List = []
+        elements_List = []
 
-		for (let i = 0; i < dataList.length; i++) {
+        for (let i = 0; i < dataList.length; i++) {
 
-			newElement = dataList[i].split(",")
-			//onsole.log(newElement)
-			elements_List.push(newElement)
+            newElement = dataList[i].split(",")
+                //onsole.log(newElement)
+            elements_List.push(newElement)
 
-		}
-
-
-
-		name_html_1();
-
-		votes_html_1();
-
-		image_src_1();
-
-		//second inline
+        }
 
 
-		name_html_2();
 
-		votes_html_2();
+        name_html_1();
 
-		image_src_2();
+        votes_html_1();
 
-	});
+        image_src_1();
+
+        //second inline
+
+
+        name_html_2();
+
+        votes_html_2();
+
+        image_src_2();
+
+    });
 };
 
 go();
@@ -49,67 +49,67 @@ var payload;
 
 function doPostRequest() {
 
-	//let payload = { name: 'SF21', year: '2022' };
-	var x = document.getElementById("myText").value;
-	var z = document.getElementById("myYear").value;
-	let y = { name: String(x), year: String(z), image: String(x) + ".jpg", votes: "0" };
-	console.log(y)
-	payload = y;
+    //let payload = { name: 'SF21', year: '2022' };
+    var x = document.getElementById("myText").value;
+    var z = document.getElementById("myYear").value;
+    let y = { name: String(x), year: String(z), image: String(x) + ".jpg", votes: "0" };
+    console.log(y)
+    payload = y;
 
-	//post
+    //post
 
-	axios.post('https://Toms-Flask-App.193ftgw.repl.co', payload);
-	console.log(payload);
-};  //entering cars
+    axios.post('https://team-willtonian1-formulawizardbackend-main-7puvzl3oma-wm.a.run.app/', payload);
+    console.log(payload);
+}; //entering cars
 
 function getRandomInt(min, max) {
-	min = Math.ceil(min);
-	max = Math.floor(max);
-	return Math.floor(Math.random() * (max - min) + min); //The maximum is exclusive and the minimum is inclusive
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min) + min); //The maximum is exclusive and the minimum is inclusive
 };
 
 function vote_One() {
 
-	//console.log(elements_List)
-	let name = document.getElementById("image1").src;
-	console.log(name)
+    //console.log(elements_List)
+    let name = document.getElementById("image1").src;
+    console.log(name)
 
-	ayo = name.replace("https://fronent.193ftgw.repl.co/images/", "")
+    ayo = name.replace("https://www.tomwilliamstech.com/formulaWizardFrontend/images/", "")
 
-	let name10 = ayo.replace(".jpg", "")
-	insert_payload = { name: String(name10) }
-	console.log(insert_payload)
-	for (let e = 0; e < elements_List.length; e++) {
+    let name10 = ayo.replace(".jpg", "")
+    insert_payload = { name: String(name10) }
+    console.log(insert_payload)
+    for (let e = 0; e < elements_List.length; e++) {
 
-		ssstr1 = elements_List[e][3]
+        ssstr1 = elements_List[e][3]
 
-		ssstr2 = ssstr1.replace("image", "")
-		ssstr3 = ssstr2.replace(/'/g, "")
-		ssstr4 = ssstr3.replace(" : ", "")
-		console.log(ssstr4)
-		console.log(ayo)
-		if (ayo == ssstr4) {
-			axios.post('https://Toms-Flask-App.193ftgw.repl.co/vote', insert_payload);
+        ssstr2 = ssstr1.replace("image", "")
+        ssstr3 = ssstr2.replace(/'/g, "")
+        ssstr4 = ssstr3.replace(" : ", "")
+        console.log(ssstr4)
+        console.log(ayo)
+        if (ayo == ssstr4) {
+            axios.post('https://team-willtonian1-formulawizardbackend-main-7puvzl3oma-wm.a.run.app/vote', insert_payload);
 
-			//reloading new content
-			name_html_1();
+            //reloading new content
+            name_html_1();
 
-			votes_html_1();
+            votes_html_1();
 
-			image_src_1();
+            image_src_1();
 
-			//second inline
-
-
-			name_html_2();
-
-			votes_html_2();
-
-			image_src_2();
+            //second inline
 
 
-		}
-	}
+            name_html_2();
+
+            votes_html_2();
+
+            image_src_2();
+
+
+        }
+    }
 
 };
 
@@ -117,160 +117,158 @@ function vote_Two() {
 
 
 
-	let name = document.getElementById("image2").src;
-	console.log(name)
+    let name = document.getElementById("image2").src;
+    console.log(name)
 
-	ayo = name.replace("https://fronent.193ftgw.repl.co/images/", "")
+    ayo = name.replace("https://www.tomwilliamstech.com/formulaWizardFrontend/images/", "")
 
-	let name10 = ayo.replace(".jpg", "")
-	insert_payload = { name: String(name10) }
-	console.log(insert_payload)
-	for (let e = 0; e < elements_List.length; e++) {
+    let name10 = ayo.replace(".jpg", "")
+    insert_payload = { name: String(name10) }
+    console.log(insert_payload)
+    for (let e = 0; e < elements_List.length; e++) {
 
-		ssstr1 = elements_List[e][3]
+        ssstr1 = elements_List[e][3]
 
-		ssstr2 = ssstr1.replace("image", "")
-		ssstr3 = ssstr2.replace(/'/g, "")
-		ssstr4 = ssstr3.replace(" : ", "")
-		console.log(ssstr4)
-		console.log(ayo)
-		if (ayo == ssstr4) {
-			axios.post('https://Toms-Flask-App.193ftgw.repl.co/vote', insert_payload);
+        ssstr2 = ssstr1.replace("image", "")
+        ssstr3 = ssstr2.replace(/'/g, "")
+        ssstr4 = ssstr3.replace(" : ", "")
+        console.log(ssstr4)
+        console.log(ayo)
+        if (ayo == ssstr4) {
+            axios.post('https://team-willtonian1-formulawizardbackend-main-7puvzl3oma-wm.a.run.app/vote', insert_payload);
 
-			//reloading new content
-			name_html_1();
+            //reloading new content
+            name_html_1();
 
-			votes_html_1();
+            votes_html_1();
 
-			image_src_1();
+            image_src_1();
 
-			//second inline
-
-
-			name_html_2();
-
-			votes_html_2();
-
-			image_src_2();
+            //second inline
 
 
-		}
-	}
+            name_html_2();
+
+            votes_html_2();
+
+            image_src_2();
+
+
+        }
+    }
 
 };
 
 var num1;
 
 function name_html_1() {
-	name1 = document.getElementById("name1")
-	num1 = getRandomInt(0, elements_List.length)
-	nom = elements_List[num1][1]
-	nom2 = nom.replace("name", "")
-	nom3 = nom2.replace(/'/g, "")
-	nom4 = nom3.replace(" : ", "")
-	name1.innerHTML = "Name: " + nom4
+    name1 = document.getElementById("name1")
+    num1 = getRandomInt(0, elements_List.length)
+    nom = elements_List[num1][1]
+    nom2 = nom.replace("name", "")
+    nom3 = nom2.replace(/'/g, "")
+    nom4 = nom3.replace(" : ", "")
+    name1.innerHTML = "Name: " + nom4
 };
 
 function name_html_2() {
-	name2 = document.getElementById("name2")
-	num2 = getRandomInt(0, elements_List.length)
+    name2 = document.getElementById("name2")
+    num2 = getRandomInt(0, elements_List.length)
 
-	while (num2 == num1) {
-		num2 = getRandomInt(0, elements_List.length)
-	}
+    while (num2 == num1) {
+        num2 = getRandomInt(0, elements_List.length)
+    }
 
-	nomm = elements_List[num2][1]
-	nomm2 = nomm.replace("name", "")
-	nomm3 = nomm2.replace(/'/g, "")
-	nomm4 = nomm3.replace(" : ", "")
-	name2.innerHTML = "Name: " + nomm4
+    nomm = elements_List[num2][1]
+    nomm2 = nomm.replace("name", "")
+    nomm3 = nomm2.replace(/'/g, "")
+    nomm4 = nomm3.replace(" : ", "")
+    name2.innerHTML = "Name: " + nomm4
 
 };
 
 function image_src_1() {
-	image1 = document.getElementById("image1")
-	sstr1 = elements_List[num1][3]
-	sstr2 = sstr1.replace("image", "")
-	sstr3 = sstr2.replace(/'/g, "")
-	sstr4 = sstr3.replace(" : ", "")
-	image1.src = "images/" + sstr4
+    image1 = document.getElementById("image1")
+    sstr1 = elements_List[num1][3]
+    sstr2 = sstr1.replace("image", "")
+    sstr3 = sstr2.replace(/'/g, "")
+    sstr4 = sstr3.replace(" : ", "")
+    image1.src = "images/" + sstr4
 
-	image1.onload
+    image1.onload
 
 
 };
 
 function image_src_2() {
-	image2 = document.getElementById("image2")
-	str1 = elements_List[num2][3]
-	str2 = str1.replace("image", "")
-	str3 = str2.replace(/'/g, "")
-	str4 = str3.replace(" : ", "")
-	image2.src = "images/" + str4
+    image2 = document.getElementById("image2")
+    str1 = elements_List[num2][3]
+    str2 = str1.replace("image", "")
+    str3 = str2.replace(/'/g, "")
+    str4 = str3.replace(" : ", "")
+    image2.src = "images/" + str4
 };
 
 function votes_html_1() {
 
-	votes1 = document.getElementById("votes1")
+    votes1 = document.getElementById("votes1")
 
-	vote1 = elements_List[num1][4].replace("votes", "")
-	vote2 = vote1.replace(/'/g, "")
-	vote3 = vote2.replace(" : ", "")
-	vote4 = vote3.replace("}", "")
-	votes1.innerHTML = " Current Votes: " + vote4
+    vote1 = elements_List[num1][4].replace("votes", "")
+    vote2 = vote1.replace(/'/g, "")
+    vote3 = vote2.replace(" : ", "")
+    vote4 = vote3.replace("}", "")
+    votes1.innerHTML = " Current Votes: " + vote4
 
 };
 
 function votes_html_2() {
 
-	votes2 = document.getElementById("votes2")
-	vote21 = elements_List[num2][4].replace("votes", "")
-	vote22 = vote21.replace(/'/g, "")
-	vote23 = vote22.replace(" : ", "")
-	votes24 = vote23.replace("}", "")
-	votes2.innerHTML = " Current Votes: " + votes24
+    votes2 = document.getElementById("votes2")
+    vote21 = elements_List[num2][4].replace("votes", "")
+    vote22 = vote21.replace(/'/g, "")
+    vote23 = vote22.replace(" : ", "")
+    votes24 = vote23.replace("}", "")
+    votes2.innerHTML = " Current Votes: " + votes24
 
 };
 
 function leaderboard() {
-	axios.get('https://Toms-Flask-App.193ftgw.repl.co/leaderboard').then(resp => {
-		data = resp.data
-	})
+    axios.get('https://Toms-Flask-App.193ftgw.repl.co/leaderboard').then(resp => {
+        data = resp.data
+    })
 }
 
 
 
 function images_sync() {
-	load += 1
-	console.log(load + ".........." + load2)
-	if (load == load2) {
+    load += 1
+    console.log(load + ".........." + load2)
+    if (load == load2) {
 
 
-		image1.style.display = 'flex';
-		image2.style.display = "flex";
-		console.log("yo")
+        image1.style.display = 'flex';
+        image2.style.display = "flex";
+        console.log("yo")
 
-	} else {
-		image1.style.display = 'none';
-		image2.style.display = "none";
-	}
+    } else {
+        image1.style.display = 'none';
+        image2.style.display = "none";
+    }
 }
 
 function images_sync2() {
-	load2 += 1
-	console.log(load + ".........." + load2)
+    load2 += 1
+    console.log(load + ".........." + load2)
 
-	if (load == load2) {
+    if (load == load2) {
 
 
-		image1.style.display = 'flex';
-		image2.style.display = "flex";
-		console.log("yo")
+        image1.style.display = 'flex';
+        image2.style.display = "flex";
+        console.log("yo")
 
-	} else {
-		image1.style.display = 'none';
-		image2.style.display = "none";
-	}
+    } else {
+        image1.style.display = 'none';
+        image2.style.display = "none";
+    }
 }
-
-
